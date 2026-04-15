077dfd3f44c98a3f9d31e34ad1d538465691d06e
Anubama <135399669+anubama-i@users.noreply.github.com>
2026-04-15 18:59:27 +0530

    Update apiPaths.js

--- frontend/vite-project/src/utils/apiPaths.js
+++ frontend/vite-project/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://expensetracker-9pxg-p6iozs6b6-anubama2006-gmailcoms-projects.vercel.app';
+export const BASE_URL = 'https://expensetracker-pni3.onrender.com';
 
 //utils/apiPaths.js
 export const API_PATHS = {
